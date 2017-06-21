0c56d7425be510b725cba567085f9fb4f8a168ff
lovevisss <530822987@qq.com>
2017-06-21 11:19:08 +0800

    add keyvalue cache

--- modules/dino_roar/src/Jurassic/RoarGenerator.php
+++ modules/dino_roar/src/Jurassic/RoarGenerator.php
@@ -3,8 +3,34 @@
 namespace Drupal\dino_roar\Jurassic;
 
 
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+
 class RoarGenerator{
-	public function getRoar($length){
-		return 'R'.str_repeat('o', $length).'AR!';
+    /**
+     * @var KeyValueFactoryInterface
+     */
+    private $keyvalue;
+
+
+    /**
+     * RoarGenerator constructor.
+     */
+    public function __construct(KeyValueFactoryInterface $keyvalue)
+    {
+
+        $this->keyvalue = $keyvalue;
+    }
+
+    public function getRoar($length){
+        $key = 'roar_'.$length;
+        $store = $this->keyvalue->get('dino');
+        if($store->has($key))
+        {
+            return store->get($key);
+        }
+        sleep(2);
+		$string =  'R'.str_repeat('o', $length).'AR!';
+		$store->set($key, $string);
+		return $string;
 	}
 }
