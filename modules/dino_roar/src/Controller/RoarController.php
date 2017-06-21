f5de4c077d059f300981820759ed811905f2ea1f
lovevisss <530822987@qq.com>
2017-06-21 09:52:32 +0800

    add service

--- modules/dino_roar/src/Controller/RoarController.php
+++ modules/dino_roar/src/Controller/RoarController.php
@@ -2,19 +2,32 @@
 
 namespace Drupal\dino_roar\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 /**
 * 
 */
-class RoarController
+class RoarController extends ControllerBase
 {
-	
-	public function roar($count)
+    private $roarGenerator;
+	public function __construct(RoarGenerator $roarGenerator)
+    {
+        $this->roarGenerator = $roarGenerator;
+    }
+
+    public function roar($count)
 	{
-		$roarGenerator = new RoarGenerator();
-		$roar = $roarGenerator->getRoar($count);
+		$roar = $this->roarGenerator->getRoar($count);
 		return new Response($roar);
 
 	}
+
+    public static function create(ContainerInterface $container)
+    {
+        $roarGenerator = $container->get('dino_roar.roar_generator');
+        return new static($roarGenerator);
+    }
+
 }
